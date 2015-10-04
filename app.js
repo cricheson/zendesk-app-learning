8533e6cda2caa5022f9bf7af2aadbde3722ab349
Christian Richeson <christian.richeson@gmail.com>
2015-10-04 16:14:39 -0500

    added request to pull suspended tickets

--- app.js
+++ app.js
@@ -2,11 +2,28 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':       'growlIt'
     },
 
-    doSomething: function() {
-    }
-  };
-
+    
+  requests: {
+    fetchsuspendedtickets: {
+      url: '/api/v2/suspended_tickets.json',
+      type: 'GET',
+    },
+  },
+    
+    getRequester: function() {
+      var currentUser = this.ticket().requester().name();
+      return currentUser;
+},
+    growlIt: function() {
+      var msg  = 'The current user is %@ :)';
+      services.notify(msg.fmt(this.getRequester()), 'error', 5 * 1000);
+      this.ajax('fetchsuspendedtickets').then(function(data){
+		    console.log(data);
+    });
+  }
+  
+};
 }());
