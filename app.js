384d82a7ce5a0239c8b80349d6d98a6d3dfd58e2
Christian Richeson <christian.richeson@gmail.com>
2015-10-04 18:14:14 -0500

    changed the get request for the suspended tickets to be able to pull by page number

--- app.js
+++ app.js
@@ -7,9 +7,11 @@
 
     
   requests: {
-    fetchsuspendedtickets: {
-      url: '/api/v2/suspended_tickets.json',
+    fetchsuspendedtickets: function(page){
+	return {
+      url: '/api/v2/suspended_tickets.json?per_page=2&page=' + page,
       type: 'GET',
+	};
     },
   },
     
@@ -20,10 +22,19 @@
     growlIt: function() {
       var msg  = 'The current user is %@ :)';
       services.notify(msg.fmt(this.getRequester()), 'error', 5 * 1000);
-      this.ajax('fetchsuspendedtickets').then(function(data){
-		    console.log(data);
+      this.ajax('fetchsuspendedtickets',1 ).then(function(data){
+		    console.log(data.count);
+		    //var pages = data.count / 3;
+	            //if (pages <= 1){
+			//function that parses data
+		    //}
+		   // else{
+			 //function that grabs the data one page at a time
+			//function that parses data
+		    //}
     });
-  }
+  },
+     
   
 };
 }());
